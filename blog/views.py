36df1deb25bafcd1d74af3f6fb7e3cd96e35e4b6
Richard-Emmanuel <richardnuelofficial@gmail.com>
2022-06-26 21:39:01 +0100

    remove tmpt_names

--- blog/views.py
+++ blog/views.py
@@ -13,20 +13,17 @@ class PostListView(ListView):
   model = Post
   def get_queryset(self):
     return super().get_queryset().filter(status="published")
-  template_name = "blog/post_list.html"
 
 
 class PostCreateView(CreateView):
   model = Post
   fields = "__all__"
   success_url  = reverse_lazy("blog:all")
-  template_name = "blog/post_form.html"
 
 
 
 class PostDetailView(DetailView):
   model = Post
-  template_name = "blog/post_detail.html"
 
   
 
@@ -34,10 +31,8 @@ class  PostUpdateView(UpdateView):
   model = Post
   fields = "__all__"
   success_url = reverse_lazy("blog:all")
-  template_name = "blog/post_form.html"
 
 class PostDeleteView(DeleteView):
   model = Post
   fields = "__all__"
   success_url = reverse_lazy("blog:all")
-  template_name = "blog/post_confirm_delete.html"
