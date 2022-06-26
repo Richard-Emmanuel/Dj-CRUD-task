4c665f1cb7f1e353378c39e5c92ea896efe25904
Richard-Emmanuel <richardnuelofficial@gmail.com>
2022-06-26 21:13:46 +0100

    modify views.py

--- blog/views.py
+++ blog/views.py
@@ -13,13 +13,13 @@ class PostListView(ListView):
   model = Post
   def get_queryset(self):
     return super().get_queryset().filter(status="published")
-template_name = "blog/post_list.html"
+  template_name = "blog/post_list.html"
 
 
 class PostCreateView(CreateView):
-  model = Post,
-  fields = "__all__",
-  success_url  = reverse_lazy("blog:all"),
+  model = Post
+  fields = "__all__"
+  success_url  = reverse_lazy("blog:all")
   template_name = "blog/post_form.html"
 
 
